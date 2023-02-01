c510cce993f94f4a3241ccec808b196ef50349ff
anaoliv <anajulia-m.oliveira@hotmail.com>
2023-01-31 21:29:37 -0300

    crud das fotografias

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class FotografiaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'legenda')
+    list_display_links = ('nome',)
+    search_fields = ('nome',)
+
+
+admin.site.register(Fotografia, FotografiaAdmin)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150)
+    descricao = models.TextField(null=False, blank=False)
+    imagem = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
